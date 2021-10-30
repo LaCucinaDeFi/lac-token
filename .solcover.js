6d55d4f4adbc38b908d2e5fbe0d4b6b68cfdaee5
Ashitosh <ashitosh@deqode.com>
2021-10-30 12:52:50 +0530

    setup project

---
+++ .solcover.js
@@ -0,0 +1,15 @@
+module.exports = {
+	skipFiles: [
+		'Migrations.sol',
+		'BaseERC721WithRoyalties.sol',
+		'Talien.sol',
+		'TraitFactory.sol',
+		'DishesNFTV2.sol',
+		'IngredientNFTV2.sol',
+		'KitchenV2.sol',
+		'OvenV2.sol',
+		'PrivateMarketplaceV2.sol',
+		'PublicMarketplaceV2.sol',
+		'SampleToken.sol'
+	]
+};
