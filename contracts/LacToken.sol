2ae41d1929d3903f7dfc45311acc2a15d11d679d
Ashitosh <ashitosh@deqode.com>
2021-11-24 13:49:44 +0530

    update LAC token

--- contracts/LacToken.sol
+++ contracts/LacToken.sol
@@ -2,29 +2,42 @@
 
 pragma solidity ^0.8.0;
 
+import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract LacToken is ERC20 {
+contract LacToken is ERC20, AccessControl {
+	bool public allowMinting;
+
 	/*
    =======================================================================
    ======================== Constructor/Initializer ======================
    =======================================================================
  	*/
-	/**
-	 * @param  _name - name of the token
-	 * @param _symbol - symbol of token
-	 * @param _preMintAddress - account address to whoch tokens will be preminted
-	 * @param _preMintAmount - indicates the amount of tokens to pre-mint
-	 */
-	constructor(
-		string memory _name,
-		string memory _symbol,
-		address _preMintAddress,
-		uint256 _preMintAmount
-	) ERC20(_name, _symbol) {
-		require(_preMintAmount > 0, 'LacToken: INVALID_PREMINT_AMOUNT');
+	constructor() ERC20('LAC', 'LaCucina Token') {
+		_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+		allowMinting = true;
+	}
 
-		//mint preMint amount of tokens to
-		_mint(_preMintAddress, _preMintAmount);
+	modifier onlyAdmin() {
+		require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), 'LacToken: ONLY_ADMIN_CAN_CALL');
+		_;
+	}
+
+	/**
+	 * @notice This method allows admin to mint the LAC tokens to account
+	 */
+	function mint(address _account, uint256 _amount) external onlyAdmin {
+		require(allowMinting, 'LacToken: MINTING_DISABLED');
+		require(_amount > 0, 'LacToken: INVALID_AMOUNT');
+
+		_mint(_account, _amount);
+	}
+
+	/**
+	 * @notice This method allows admin to disable the minting of tokens
+	 */
+	function disableMinting() external onlyAdmin {
+		require(allowMinting, 'LacToken: ALREADY_DISABLED');
+		allowMinting = false;
 	}
 }
